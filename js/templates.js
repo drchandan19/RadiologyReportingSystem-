0e5e237ed527674911dd9b9e972087dc6c070842
drchandan19 <drchandantiwari19@gmail.com>
2026-06-18 17:25:51 +0530

    Add MRI Brain template to templates.js

--- js/templates.js
+++ js/templates.js
@@ -1,1 +1,22 @@
+const templates = {
 
+    MRI: {
+
+        "MRI Brain": `MRI BRAIN
+
+No focal parenchymal signal abnormality.
+
+No diffusion restriction.
+
+No intracranial hemorrhage.
+
+Ventricular system is normal.
+
+No midline shift.
+
+Impression:
+No significant intracranial abnormality detected.`
+
+    }
+
+};
