c660bbc8dd5035e3c24d287b0bc9f0ca8d7510a9
drchandan19 <drchandantiwari19@gmail.com>
2026-06-18 16:11:34 +0530

    Add CT report generation for chest and head studies

--- js/script.js
+++ js/script.js
@@ -116,6 +116,47 @@ No hydrocele or varicocele seen.
 Impression:
 No significant abnormality detected.`;
 
+         }
+
+    }
+
+    else if (modality === "CT") {
+
+        if (studyType === "CT Chest") {
+
+            report =
+`CT CHEST
+
+Lungs:
+No focal consolidation or suspicious pulmonary nodule.
+
+Pleura:
+No pleural effusion or pneumothorax.
+
+Mediastinum:
+No significant lymphadenopathy.
+
+Impression:
+No acute CT abnormality detected.`;
+
+        }
+
+        else if (studyType === "NCCT Head") {
+
+            report =
+`NCCT HEAD
+
+No evidence of intracranial hemorrhage.
+
+No focal parenchymal abnormality.
+
+Ventricular system is normal.
+
+No midline shift.
+
+Impression:
+No acute intracranial abnormality detected.`;
+
         }
 
     }
