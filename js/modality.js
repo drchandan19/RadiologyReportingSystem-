91fcc572f9ad8f37d4e966885066414d2bed8d05
drchandan19 <drchandantiwari19@gmail.com>
2026-06-18 16:02:33 +0530

    Implement modality change event for study types
    Add event listener to update study types based on modality selection.

--- js/modality.js
+++ js/modality.js
@@ -1,1 +1,31 @@
+document.getElementById("modality").addEventListener("change", function () {
 
+    let modality = this.value;
+
+    let studyType = document.getElementById("studyType");
+
+    studyType.innerHTML = "";
+
+    if (modality === "Ultrasound") {
+
+        studyType.innerHTML = `
+            <option>Abdomen</option>
+            <option>KUB</option>
+            <option>Pelvis</option>
+            <option>Thyroid</option>
+            <option>Breast</option>
+            <option>Scrotum</option>
+        `;
+
+    }
+
+    else if (modality === "CT") {
+
+        studyType.innerHTML = `
+            <option>CT Chest</option>
+            <option>NCCT Head</option>
+        `;
+
+    }
+
+});
